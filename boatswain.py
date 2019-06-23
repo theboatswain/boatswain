e92314ff2c3f3273fbcc115eb1d3e5bac15d8d59
Manh Tu <glmanhtu@gmail.com>
2019-06-23 19:59:16 +0100

    first commit

---
+++ boatswain.py
@@ -0,0 +1,29 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow
+
+
+class Boatswain(QMainWindow):
+    def __init__(self):
+        super(Boatswain, self).__init__()
+        uic.loadUi('boatswain.ui', self)
+
+        self.show()
+        self.Add.clicked.connect(self.addInputTextToListbox)
+
+
+    def addInputTextToListbox(self):
+        print("asdasd")
+
+    def mousePressEvent(self, event):
+        focused_widget = QApplication.focusWidget()
+        if isinstance(focused_widget, QLineEdit):
+            focused_widget.clearFocus()
+        QMainWindow.mousePressEvent(self, event)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Boatswain()
+    sys.exit(app.exec_())
