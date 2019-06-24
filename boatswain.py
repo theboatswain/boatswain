cb6988b53789a4546676bc92ab598c143aee7f1e
Manh Tu <glmanhtu@gmail.com>
2019-06-24 07:28:53 +0100

    Add icon

--- boatswain.py
+++ boatswain.py
@@ -1,7 +1,8 @@
 import sys
 
 from PyQt5 import uic
-from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtCore import pyqtSlot, QSize
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QLineEdit, QMainWindow, QDialog
 
 from add.add_app import AddAppDialog
@@ -32,5 +33,12 @@ class Boatswain(QMainWindow):
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
+    app_icon = QIcon()
+    app_icon.addFile('resources/icon/boatswain-16x16.png', QSize(16, 16))
+    app_icon.addFile('resources/icon/boatswain-24x24.png', QSize(24, 24))
+    app_icon.addFile('resources/icon/boatswain-32x32.png', QSize(32, 32))
+    app_icon.addFile('resources/icon/boatswain-48x48.png', QSize(48, 48))
+    app_icon.addFile('resources/icon/boatswain-256x256.png', QSize(256, 256))
+    app.setWindowIcon(app_icon)
     window = Boatswain()
     sys.exit(app.exec_())
