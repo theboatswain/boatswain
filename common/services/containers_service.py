014be0816442967467617da4218553000d14c221
Manh Tu <glmanhtu@gmail.com>
2019-06-29 21:53:48 +0100

    Those two services will be use in multiple modules, so, sould be in common/services

--- common/services/containers_service.py
+++ common/services/containers_service.py
@@ -1,9 +1,9 @@
 from common.models.container import Container
-from home import data_transporter_service
+from common.services import data_transporter_service
 from common.utils.constants import CONTAINER_CHANNEL
 
 
-def install_container(image_name, description):
+def installContainer(image_name, description):
     container = Container(name=image_name, description=description, tag='latest', status=0)
     container.save()
     data_transporter_service.fire(CONTAINER_CHANNEL, container)
