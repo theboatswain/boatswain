73c43696acc1f988383051defb99de830e140cff
Dung Le <lqdung17.02@gmail.com>
2019-07-06 16:41:49 +0700

    [UPDATE] update for container model object - Add field description - Add image_name as a new field

--- common/models/container.py
+++ common/models/container.py
@@ -7,9 +7,16 @@ class Container(BaseModel):
     """ Model object represents for a docker container"""
 
     id = IntegerField(primary_key=True)
+    # docker container name
     name = CharField()
+    # image name, ex: mongo
+    image_name = CharField()
+    # image tag, ex: 1.0
     tag = CharField()
+    # a short info that describes about app/soft/tool...
     description = TextField()
+    # installed/removed
     status = IntegerField()
     avatar = CharField(default='')
+    # docker registry, default is docker hub, users can also use a custom registry
     repo = CharField(default='dockerhub')
