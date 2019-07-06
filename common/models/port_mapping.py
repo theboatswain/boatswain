12ec037d977844279a7c0c4ed17a966871d166c3
Dung Le <lqdung17.02@gmail.com>
2019-07-06 16:42:10 +0700

    [UPDATE] update for port_mapping model object - Add field description - Add protocol as a new field

--- common/models/port_mapping.py
+++ common/models/port_mapping.py
@@ -8,6 +8,11 @@ class PortMapping(BaseModel):
     """ Model base for port mapping info """
 
     id = IntegerField(primary_key=True)
+    # target container Id
     container = ForeignKeyField(Container, backref='ports')
+    # client port
     port = IntegerField()
+    # protocol is either tcp, udp, or sctp.
+    protocol = CharField()
+    # container port
     targetPort = IntegerField()
