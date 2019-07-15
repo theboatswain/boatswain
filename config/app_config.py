f90ffb03bb38766d8bdf45d9f8411a70d8d971cd
Tu Manh Vu <glmanhtu@gmail.com>
2019-07-15 17:17:08 +0100

    Added license to every files

--- config/app_config.py
+++ config/app_config.py
@@ -1,8 +1,25 @@
+#  This file is part of Boatswain.
+#
+#      Boatswain is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      Boatswain is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
 from PyQt5.QtCore import QCoreApplication, Qt, QSize
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QTabWidget, QDialog
 
 from common.models.container import Container
-from common.utils.custom_ui import BQSizePolicy, AutoResizeWidget
+from common.utils.custom_ui import BQSizePolicy
 from config.configures.environment import EnvironmentVariable
 from config.configures.general import GeneralAppConfig
 from config.configures.port_mapping import PortMappingConfig
--- config/models/user_env_model.py
+++ config/models/user_env_model.py
@@ -1,10 +1,27 @@
+#  This file is part of Boatswain.
+#
+#      Boatswain is free software: you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation, either version 3 of the License, or
+#      (at your option) any later version.
+#
+#      Boatswain is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#
+#      You should have received a copy of the GNU General Public License
+#      along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+#
+#
+
 from PyQt5.QtCore import QAbstractTableModel
 from PyQt5.QtCore import QModelIndex
 from PyQt5.QtCore import QVariant
 from PyQt5.QtCore import Qt
 
 from common.models.container import Container
-from common.services import containers_service, config_service
+from common.services import config_service
 from common.utils.constants import CONTAINER_CONF_CHANGED
 
 
