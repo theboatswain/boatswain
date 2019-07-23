f0a9de067fe1187673904f0fcfce5704db76d60b
Manh Tu <glmanhtu@gmail.com>
2019-07-23 07:43:12 +0100

    [OPTIMISE] Optimise UI

--- boatswain/home/advanced/advanced_app_widget.py
+++ boatswain/home/advanced/advanced_app_widget.py
@@ -21,7 +21,7 @@ from PyQt5.QtWidgets import QLabel, QComboBox, QSizePolicy, QWidget, QLineEdit, 
 from boatswain.common.models.container import Container
 from boatswain.common.models.preferences_shortcut import PreferencesShortcut
 from boatswain.common.models.tag import Tag
-from boatswain.common.services import config_service
+from boatswain.common.services import config_service, containers_service
 from boatswain.common.utils.constants import CONTAINER_CONF_CHANGED
 from boatswain.common.utils.custom_ui import BQSizePolicy
 from boatswain.config.app_config import AppConfig
@@ -32,12 +32,14 @@ class AdvancedAppWidget:
     _translate = QCoreApplication.translate
     template = 'AdvancedAppWidget'
     animation: QPropertyAnimation
+    tags: QComboBox
 
     def __init__(self, parent, container: Container) -> None:
         self.container = container
         self.ui = AdvancedAppWidgetUi(parent, container)
         self.ui.advanced_configuration.clicked.connect(self.onAdvancedConfigurationClicked)
         self.drawShortcuts()
+        containers_service.listen(self.container, 'tag_index', self.listenTagChange)
 
     def onImageTagChange(self, full_tag_name):
         if not full_tag_name:
@@ -66,43 +68,47 @@ class AdvancedAppWidget:
             self.animation.setEndValue(0)
             self.animation.start()
 
-    def drawShortcuts(self):
-        row = 0
-        shortcuts = PreferencesShortcut.select().where(PreferencesShortcut.container == self.container)
-        for shortcut in shortcuts:
-            label = QLabel(self.ui.widget)
-            label.setText(self._translate(self.template, shortcut.label) + ':')
-            self.ui.grid_layout.addWidget(label, row, 0, 1, 1)
-            input_box = QLineEdit(self.ui.widget)
-            input_box.setText(shortcut.default_value)
-            input_box.setStyleSheet('border: none; background-color: transparent')
-            self.ui.grid_layout.addWidget(input_box, row, 1, 1, 2)
-            if shortcut.pref_type in ['File', 'Folder']:
-                finder = QPushButton(self.ui.widget)
-                finder.setText('...')
-                finder.setMaximumWidth(40)
-                self.ui.grid_layout.addWidget(finder, row, 3, 1, 2)
-            else:
-                hidden_widget = QWidget(self.ui.widget)
-                hidden_widget.setSizePolicy(BQSizePolicy(h_stretch=1))
-                self.ui.grid_layout.addWidget(hidden_widget, row, 3, 1, 2)
-            row += 1
+    def drawShortcut(self, shortcut: PreferencesShortcut, row):
+        label = QLabel(self.ui.widget)
+        label.setText(self._translate(self.template, shortcut.label) + ':')
+        self.ui.grid_layout.addWidget(label, row, 0, 1, 1)
+        input_box = QLineEdit(self.ui.widget)
+        input_box.setText(shortcut.default_value)
+        input_box.setStyleSheet('border: none; background-color: transparent')
+        self.ui.grid_layout.addWidget(input_box, row, 1, 1, 2)
+        if shortcut.pref_type in ['File', 'Folder']:
+            finder = QPushButton(self.ui.widget)
+            finder.setText('...')
+            finder.setMaximumWidth(40)
+            self.ui.grid_layout.addWidget(finder, row, 3, 1, 2)
+        else:
+            hidden_widget = QWidget(self.ui.widget)
+            hidden_widget.setSizePolicy(BQSizePolicy(h_stretch=1))
+            self.ui.grid_layout.addWidget(hidden_widget, row, 3, 1, 2)
 
+    def drawTagShortcut(self, row):
         label = QLabel(self.ui.widget)
         self.ui.grid_layout.addWidget(label, row, 0, 1, 1)
-        tags = QComboBox(self.ui.widget)
-        tags.setSizePolicy(BQSizePolicy(h_stretch=2, height=QSizePolicy.Fixed))
-        self.ui.grid_layout.addWidget(tags, row, 1, 1, 2)
+        self.tags = QComboBox(self.ui.widget)
+        self.tags.setSizePolicy(BQSizePolicy(h_stretch=2, height=QSizePolicy.Fixed))
+        self.ui.grid_layout.addWidget(self.tags, row, 1, 1, 2)
         hidden_widget = QWidget(self.ui.widget)
         hidden_widget.setSizePolicy(BQSizePolicy(h_stretch=1))
         self.ui.grid_layout.addWidget(hidden_widget, row, 3, 1, 2)
 
         for index, tag in enumerate(Tag.select().where(Tag.container == self.container)):
-            tags.addItem(self.container.image_name + ":" + tag.name)
+            self.tags.addItem(self.container.image_name + ":" + tag.name)
             if tag.name == self.container.tag:
-                tags.setCurrentIndex(index)
-        tags.currentTextChanged.connect(self.onImageTagChange)
+                self.tags.setCurrentIndex(index)
+        self.tags.currentTextChanged.connect(self.onImageTagChange)
         label.setText(self._translate(self.template, "Image tag:"))
+
+    def drawShortcuts(self):
+        shortcuts = PreferencesShortcut.select().where(PreferencesShortcut.container == self.container)
+        for index, shortcut in enumerate(shortcuts):
+            self.drawShortcut(shortcut, index)
+        self.drawTagShortcut(len(shortcuts))
+
         self.ui.setMaximumHeight(9999999)
         self.app_info_max_height = self.ui.sizeHint().height() + 10
         self.ui.setMaximumHeight(0)
@@ -111,3 +117,6 @@ class AdvancedAppWidget:
         while self.ui.grid_layout.count():
             item = self.ui.grid_layout.takeAt(0)
             item.widget().deleteLater()
+
+    def listenTagChange(self, index):
+        self.tags.setCurrentIndex(index)
