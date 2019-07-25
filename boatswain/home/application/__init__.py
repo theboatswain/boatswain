306673042cc5ce8b8550ae741234721d59d95cc3
Manh Tu <glmanhtu@gmail.com>
2019-07-25 07:44:24 +0100

    [FIXED] license

--- boatswain/home/application/__init__.py
+++ boatswain/home/application/__init__.py
@@ -11,6 +11,6 @@
 #      GNU General Public License for more details.
 #
 #      You should have received a copy of the GNU General Public License
-#      along with Foobar.  If not, see <https://www.gnu.org/licenses/>.
+#      along with Boatswain.  If not, see <https://www.gnu.org/licenses/>.
 #
 #
